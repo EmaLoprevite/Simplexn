38d915977bca85f7fb6631d1ab81223fc5dd8de7
Emanuele <35506967+EmaLoprevite@users.noreply.github.com>
2018-02-01 14:36:14 +0100

    Check quads2tria

--- src/Simplexn.jl
+++ src/Simplexn.jl
@@ -56,66 +56,61 @@ end
 #map(x -> tuple(x...) ,[[0, 1], [0, 4], [1, 2]])
 
 # Transformation to triangles by sorting circularly the vertices of faces
-function quads2tria(model)												# PROBLEM!!!
-	counter = 0			# REMOVE
+function quads2tria(model)
 	V, FV = model
 	out = Array{Int64,1}[]
 	nverts = length(V)-1
-	println("nverts:\n", nverts)
+	#println("nverts:\n", nverts)
 	for face in FV
-		println("face\n", face)
+		#println("face\n", face)
 		arr = [V[v+1] for v in face]
-		println("arr\n", arr)
+		#println("arr\n", arr)
 		centroid = sum(arr)/length(arr)
-		println("centroid\n", centroid)
+		#println("centroid\n", centroid)
 		append!(V,[centroid])
-		println("V\n", V)
+		#println("V\n", V)
 		nverts += 1
-		println("nverts\n", nverts)
-		println("face[1]\n",face[1])
-		println("face[2]\n",face[2])
-		v1, v2 = V[face[1]+1]-centroid, V[face[2]+1]-centroid			# GENERALIZE!!!
-		println("v1\n", v1)
-		println("v2\n", v2)
+		#println("nverts\n", nverts)
+		#println("face[1]\n",face[1])
+		#println("face[2]\n",face[2])
+		v1, v2 = V[face[1]+1]-centroid, V[face[2]+1]-centroid
+		#println("v1\n", v1)
+		#println("v2\n", v2)
 		v3 = cross(v1,v2)
-		println("v3\n", v3)
-		if norm(v3) < 1/(10^3)
-			v1, v2 = V[face[1]+1]-centroid, V[face[3]+1]-centroid		# GENERALIZE!!!
-			println("v1\n", v1)
-			println("v2\n", v2)
+		#println("v3\n", v3)
+		if norm(v3) < 1/(10^3)										# mettere if o while?
+			v1, v2 = V[face[1]+1]-centroid, V[face[3]+1]-centroid
+			#println("v1\n", v1)
+			#println("v2\n", v2)
 			v3 = cross(v1,v2)
-			println("v3\n", v3)
+			#println("v3\n", v3)
 		end
 		transf = inv(hcat(v1,v2,v3)')
-		println("transf\n",transf)
+		#println("transf\n",transf)
 		verts = [(V[v+1]'*transf)'[1:end-1] for v in face]
-		println("verts\n",verts)
+		#println("verts\n",verts)
 		tcentroid = sum(verts)/length(verts)
-		println("tcentroid\n",tcentroid)
-		println(typeof(tcentroid))
-		tverts = [v-tcentroid for v in verts]							# PROBLEM FROM HERE...
-		println("tverts\n",tverts)
+		#println("tcentroid\n",tcentroid)
+		#println(typeof(tcentroid))
+		tverts = [v-tcentroid for v in verts]
+		#println("tverts\n",tverts)
 		iterator = collect(zip(tverts, face))
-		println("iterator\n",iterator)
+		#println("iterator\n",iterator)
 		rverts = [ [atan2(reverse(iterator[i][1])...), iterator[i][2]] for i in 1:length(iterator) ]
-		println("rverts\n",rverts)
-		rvertss = sort(rverts, lt=(x,y)->isless(x[1],y[1]))				# ... TO HERE!!!
-		println("rvertss\n",rvertss)
+		#println("rverts\n",rverts)
+		rvertss = sort(rverts, lt=(x,y)->isless(x[1],y[1]))
+		#println("rvertss\n",rvertss)
 		ord = [pair[2] for pair in rvertss]
-		println("ord 1\n",ord)
+		#println("ord 1\n",ord)
 		append!(ord,ord[1])
-		println("ord 2\n",ord)
+		#println("ord 2\n",ord)
 		edges = [ [i[2], ord[i[1]+1]] for i in enumerate(ord[1:end-1]) ]
-		println("edges\n",edges)
+		#println("edges\n",edges)
 		triangles = [ prepend!(edge,nverts) for edge in edges ]
-		println("triangles\n",triangles)
+		#println("triangles\n",triangles)
 		append!(out,triangles)
-		println("out\n",out)
-		println("V\n",V)
-		counter+=1			# REMOVE
-		if counter == 3		# REMOVE
-			break
-		end
+		#println("out\n",out)
+		#println("V\n",V)
 	end
 	return V, out
 end
