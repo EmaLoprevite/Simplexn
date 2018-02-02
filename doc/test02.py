9225aeefb10191b03c0b1b26dde6122b5714cfc1
Emanuele <35506967+EmaLoprevite@users.noreply.github.com>
2018-02-02 18:28:23 +0100

    Minor changes

--- doc/test02.py
+++ doc/test02.py
@@ -2,14 +2,11 @@
 from larlib import *
 
 V,CV = larSimplexGrid1([2,2,2])
-print "\nV:\n",V,"\nCV:\n",CV
-#VIEW(EXPLODE(1.5,1.5,1.5)(MKPOLS((V,CV))))
+VIEW(EXPLODE(1.5,1.5,1.5)(MKPOLS((V,CV))))
 
 
 FV = larSimplexFacets(CV)
-print "\nFV:\n",FV
-#VIEW(EXPLODE(1.5,1.5,1.5)(MKPOLS((V,FV))))
+VIEW(EXPLODE(1.5,1.5,1.5)(MKPOLS((V,FV))))
 EV = larSimplexFacets(FV)
-print "\nEV:\n",EV
-#VIEW(EXPLODE(1.5,1.5,1.5)(MKPOLS((V,EV))))
+VIEW(EXPLODE(1.5,1.5,1.5)(MKPOLS((V,EV))))
 
