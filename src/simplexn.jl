efaa729c0f7a6cee1796315a7e8c51046bfbe5e5
Emanuele <35506967+EmaLoprevite@users.noreply.github.com>
2018-02-22 17:51:50 +0100

    Trying parallelization

--- src/simplexn.jl
+++ src/simplexn.jl
@@ -9,58 +9,61 @@
 VOID = V0,CV0 = [Int64[]],[[0]]		# the empty simplicial model
 
 # Generation of the output model vertices in a multiple extrusion of a LAR model
-function larExtrude1(model::Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}, pattern::Array{Int64,1})
+@everywhere function larExtrude1(model::Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}, pattern::Array{Int64,1})
 	V, FV = model
 	d, m = length(FV[1]), length(pattern)
 	#@everywhere d, m = length(FV[1]), length(pattern)
 	coords = cumsum(append!([0],abs.(pattern)))			# built-in function cumsum
-	println("spawn")
+	#println("spawn")
 	outVertices = @spawn [vcat(v,z) for z in coords for v in V]
-	#offset, rangelimit = length(V), d*m
-	#@everywhere outcells = Array{Int64}(m,0)
-	offset, outcells, rangelimit = length(V), SharedArray{Int64}(m,0), d*m
+	offset, outcells, rangelimit = length(V), Array{Int64}(m,0), d*m
+	#@everywhere
+	#offset, outcells, rangelimit = length(V), SharedArray{Int64}(m,0), d*m
 	for cell in FV
 		tube = [v + k*offset for k in 0:m for v in cell]
 		celltube = Int64[]
 		#@sync @parallel for k in 1:rangelimit
 		#	append!(celltube,tube[k:k+d])
 		#end
-		println("sync parallel")
+		#println("sync parallel")
 		celltube = @parallel (append!) for k in 1:rangelimit
 			tube[k:k+d]
 		end
-		println("end sync parallel")
+		#println("end sync parallel")
 		outcells = hcat(outcells,permutedims(reshape(celltube,d*(d+1),m),[2,1]))	# PARALLELING?
 	end
-	#@everywhere cellGroups = Int64[]
-	cellGroups = SharedArray{Int64}(0)
-	@async for k in 1:m
+	#@everywhere 
+	cellGroups = Int64[]
+	#cellGroups = SharedArray{Int64}(0)
+	#tic()
+	for k in 1:m
 		if pattern[k]>0
 			cellGroups = vcat(cellGroups,outcells[k,:])
 		end
 	end
+	#toc()
 	#cellGroups = @parallel (vcat) for k in 1:m
 	#	if pattern[k]>0
 	#		outcells[k,:]
 	#	end
 	#end
-	#println("\ncellgroup:\n",cellGroups,"\ntype: ",typeof(cellGroups))
+	##println("\ncellgroup:\n",cellGroups,"\ntype: ",typeof(cellGroups))
 	#outVertices = [vcat(v,z) for z in coords for v in V]
 	outCellGroups = Array{Int64,1}[]
 	#for k in 1:d+1:length(cellGroups)
 	#	append!(outCellGroups,[cellGroups[k:k+d]])
 	#end
-	println("parallel 2")
+	#println("parallel 2")
 	outCellGroups = @parallel (append!) for k in 1:d+1:length(cellGroups)
 			[cellGroups[k:k+d]]
 	end
-	println("end parallel 2")
-	println("return")
+	#println("end parallel 2")
+	#println("return")
 	return fetch(outVertices), outCellGroups
 end
 
 # Generation of simplicial grids of any dimension and shape
-function larSimplexGrid1(shape::Array{Int64,1})
+@everywhere function larSimplexGrid1(shape::Array{Int64,1})
 	model = VOID
 	for item in shape											# NO PARALLELING
 		model = larExtrude1(model,repmat([1],item))
@@ -69,17 +72,17 @@ function larSimplexGrid1(shape::Array{Int64,1})
 end
 
 # Extraction of non-oriented (d−1)-facets of d-dimensional simplices
-function larSimplexFacets(simplices::Array{Array{Int64,1},1})	# returns array of arrays and not array of tuples
+@everywhere function larSimplexFacets(simplices::Array{Array{Int64,1},1})	# returns array of arrays and not array of tuples
 	#@everywhere out = Array{Int64,1}[]
 	out = Array{Int64,1}[]
     d = length(simplices[1])
-    println("parallel 1")
-    tic()
+    #println("parallel 1")
+    #tic()
     out = @parallel (append!) for simplex in simplices			# WTF, IT TAKES LONGER...!!!
     		collect(combinations(simplex,d-1))		# combinations() needs pkg Combinatorics everywhere
     	end
-    toc()
-    println("fine parallel 1")
+    #toc()
+    #println("fine parallel 1")
     #for simplex in simplices
     #	append!(out,collect(combinations(simplex,d-1)))
     #end
@@ -88,7 +91,7 @@ end
 #map(x->tuple(x...),[[0, 1],[0, 4],[1, 2]])
 
 # Transformation to triangles by sorting circularly the vertices of faces
-function quads2tria(model::Tuple{Array{Array{Float64,1},1},Array{Array{Int64,1},1}})
+@everywhere function quads2tria(model::Tuple{Array{Array{Float64,1},1},Array{Array{Int64,1},1}})
 	V, FV = model
 	#@everywhere out = Array{Int64,1}[]
 	out = Array{Int64,1}[]
