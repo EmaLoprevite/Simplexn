8d22792d050b74d6247c8a0e428223538003e2bd
Emanuele <35506967+EmaLoprevite@users.noreply.github.com>
2018-07-21 21:55:52 +0100

    Add two functions

--- test/speedup.jl
+++ test/speedup.jl
@@ -4,3 +4,17 @@
 ### Authors: Fabio Fatelli, Emanuele Loprevite
 #####################################################
 
+function timing(f,x,N::UInt8) # f::Function ?
+	t = Array{Float64}(N)
+	f(x) # or x... ?
+	for k in 1:N
+		t[k] = @elapsed f(x)
+	end
+	return mean(t)
+end
+
+function plotting(name::String,timeS::Array{Float64,1},timeP::Array{Float64,1})
+	plot(timeS,title=name,label="Serial",xlabel="Input",ylabel="Time (seconds)")
+	plot(timeP,title=name,label="Parallel",xlabel="Input",ylabel="Time (seconds)")
+	plot(,title=name,label=["Serial","Parallel"],xlabel="Input",ylabel="Time (seconds)")
+end
