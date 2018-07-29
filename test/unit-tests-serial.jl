4a86e385d4f824ca6d3807e690f79892e209e9dc
Emanuele <35506967+EmaLoprevite@users.noreply.github.com>
2018-07-30 00:15:02 +0100

    More tests larExtrude1

--- test/unit-tests-serial.jl
+++ test/unit-tests-serial.jl
@@ -7,10 +7,10 @@
 include("../src/simplexn-serial.jl")
 
 using Base.Test
-using LARVIEW
+#using LARVIEW
 
-using PyCall
-@pyimport larlib as p
+#using PyCall
+#@pyimport larlib as p
 
 VOID = [Int64[]],[[0]] # the empty simplicial model
 
@@ -20,38 +20,60 @@ VOID = [Int64[]],[[0]] # the empty simplicial model
 		pattern = [1]
 		model = larExtrude1(inModel,pattern)
 		println(model)
+		@test typeof(model) == Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
 		@test length(model[1]) == length(pattern)+1
+		@test length(model[2]) == length(filter(x->x>0,pattern))
 		#LARVIEW.viewexploded(hcat(model[1]...),model[2]+1)
 		
 		pattern = [1,3]
 		model = larExtrude1(inModel,pattern)
 		println(model)
+		@test typeof(model) == Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
 		@test length(model[1]) == length(pattern)+1
+		@test length(model[2]) == length(filter(x->x>0,pattern))
 		#LARVIEW.viewexploded(hcat(model[1]...),model[2]+1)
 		
 		pattern = [1,3,-2]
 		model = larExtrude1(inModel,pattern)
 		println(model)
+		@test typeof(model) == Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
 		@test length(model[1]) == length(pattern)+1
+		@test length(model[2]) == length(filter(x->x>0,pattern))
 		#LARVIEW.viewexploded(hcat(model[1]...),model[2]+1)
 		
 		inModel = ([[0,0],[0,1]],[[1,0],[1,1]])
 		pattern = [1]
 		model = larExtrude1(inModel,pattern)
 		println(model)
+		@test typeof(model) == Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
 		@test length(model[1]) == 4+2*(length(pattern)-1)
+		@test length(model[2]) == 4*length(filter(x->x>0,pattern))
 		#LARVIEW.viewexploded(hcat(model[1]...),model[2]+1)
 		
 		pattern = [1,3]
 		model = larExtrude1(inModel,pattern)
 		println(model)
+		@test typeof(model) == Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
 		@test length(model[1]) == 4+2*(length(pattern)-1)
+		@test length(model[2]) == 4*length(filter(x->x>0,pattern))
 		#LARVIEW.viewexploded(hcat(model[1]...),model[2]+1)
 		
 		pattern = [1,3,-2]
 		model = larExtrude1(inModel,pattern)
 		println(model)
+		@test typeof(model) == Tuple{Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
 		@test length(model[1]) == 4+2*(length(pattern)-1)
+		@test length(model[2]) == 4*length(filter(x->x>0,pattern))
+		#LARVIEW.viewexploded(hcat(model[1]...),model[2]+1)
+		
+		inModel = ([[0.0,0],[0,1]],[[1,0],[1,1]])
+		@test typeof(inModel[1]) == Array{Array{Float64,1},1}
+		pattern = [1]
+		model = larExtrude1(inModel,pattern)
+		println(model)
+		@test typeof(model) == Tuple{Array{Array{Float64,1},1},Array{Array{Int64,1},1}}
+		@test length(model[1]) == 4+2*(length(pattern)-1)
+		@test length(model[2]) == 4*length(filter(x->x>0,pattern))
 		#LARVIEW.viewexploded(hcat(model[1]...),model[2]+1)
 	end
 	@testset "larSimplexGrid1" begin
