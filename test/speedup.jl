1c7484c565318ea50c99f1b696b31d370a63ee0f
Emanuele <35506967+EmaLoprevite@users.noreply.github.com>
2018-08-01 02:02:28 +0100

    PDF and images

--- test/speedup.jl
+++ test/speedup.jl
@@ -7,8 +7,6 @@
 include("../src/simplexn-serial.jl")
 include("../src/simplexn-parallel.jl")
 
-addprocs(11)
-
 using Plots
 Plots.scalefontsizes(0.7)
 gr() # loading backend
@@ -31,11 +29,11 @@ function plotting(name::String,timeS::Array{Float64,1},timeP::Array{Float64,1},f
 	l = max(length(timeS),length(timeP))+1
 	pathName = "../doc/tex/figures/"*name
 	s, p, xlb, ylb, DPI = "Serial", "Parallel", "Input", "Time (seconds)", 150
-	plot(timeS,xlims=(1,l),title=name,label=s,xlabel=xlb,ylabel=ylb,dpi=DPI)
+	plot(timeS,xlims=(1,l),title=name,label=s,xlabel=xlb,ylabel=ylb,dpi=DPI,legend=:topleft)
 	savefig(pathName*"serial")
-	plot(timeP,xlims=(1,l),title=name,label=p,xlabel=xlb,ylabel=ylb,dpi=DPI)
+	plot(timeP,xlims=(1,l),title=name,label=p,xlabel=xlb,ylabel=ylb,dpi=DPI,legend=:topleft)
 	savefig(pathName*"parallel")
-	plot([timeS,timeP],xlims=(1,l),title=name,label=[s,p],xlabel=xlb,ylabel=ylb,dpi=DPI)
+	plot([timeS,timeP],xlims=(1,l),title=name,label=[s,p],xlabel=xlb,ylabel=ylb,dpi=DPI,legend=:topleft)
 	savefig(pathName*"both")
 end
 
@@ -47,39 +45,27 @@ function plotting(name::String,timeS::Array{Float64,1},timeP::Array{Float64,1})
 	p2 = plot(timeP,label=p)
 	p3 = plot([timeS,timeP],label=[s,p])
 	plot(p1,p2,p3,xlims=(1,l),xlabel=xlb,ylabel=ylb,dpi=1.5*DPI,legend=:topleft)
-	pathName = "../doc/tex/figures/"*name*"All"
-	savefig(pathName)
 end
 
 timeSer = [timing(larExtrude1,[VOID,repmat([1,2,3,4],k^4)],nt) for k in 1:2*N]
 timePar = [timing(plarExtrude1,[VOID,repmat([1,2,3,4],k^4)],nt) for k in 1:2*N]
-#plotting("larExtrude1",timeSer,timePar)
-println("larExtrude1\n","serial\n",timeSer,"\nparallel\n",timePar)
+plotting("larExtrude1",timeSer,timePar)
 
-simp = [collect(1:500),collect(501:1000),collect(1001:1500)]
+simp = [collect(1:500)]
 sLen = length(simp[1])
-for k in 2:5*N
+for k in 2:2*N^2
 	push!(simp,simp[end]+sLen)
 end
-timeSer = [timing(larSimplexFacets,[simp[1:k]],nt) for k in 1:5*N]
-timePar = [timing(plarSimplexFacets,[simp[1:k]],nt) for k in 1:5*N]
-#plotting("larSimplexFacets",timeSer,timePar)
-println("larSimplexFacets\n","serial\n",timeSer,"\nparallel\n",timePar)
+timeSer = [timing(larSimplexFacets,[simp[1:k]],nt) for k in 1:2*N^2]
+timePar = [timing(plarSimplexFacets,[simp[1:k]],nt) for k in 1:2*N^2]
+plotting("larSimplexFacets",timeSer,timePar)
 
-#=
-a=repmat([1],77)
-model = plarExtrude1(VOID,a)
-model = plarExtrude1(model,a)
-model = plarExtrude1(model,a)
-verts, quads = model
-=#
-verts, quads = [[0,0,0],[0,1,0],[1,0,0],[1,1,0]], [[0,1,2,3]]
+verts, quads = [[0,0,0],[0,1,0],[1,0,0],[1,1,0],[2,2,0],[2,3,0],[3,2,0],[3,3,0]], [[0,1,2,3],[4,5,6,7]]
 len = length(quads[1])
-for k in 2:7*N
-	append!(verts,verts[end-len+1:end]+11)
+for k in 2:2*N^2
+	append!(verts,verts[end-len+1:end]+1)
 	append!(quads,[quads[end]+len])
 end
-timeSer = [timing(quads2tria,[(verts[1:len*k],quads[1:k])],nt) for k in 1:7*N]
-timePar = [timing(pquads2tria,[(verts[1:len*k],quads[1:k])],nt) for k in 1:7*N]
-#plotting("quads2tria",timeSer,timePar)
-println("quads2tria\n","serial\n",timeSer,"\nparallel\n",timePar)
+timeSer = [timing(quads2tria,[(verts[1:len*k],quads[1:k])],nt) for k in 1:2*N^2]
+timePar = [timing(pquads2tria,[(verts[1:len*k],quads[1:k])],nt) for k in 1:2*N^2]
+plotting("quads2tria",timeSer,timePar)
